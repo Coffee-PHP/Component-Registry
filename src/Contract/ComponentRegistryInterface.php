41be7ea261967f89c551535f21874b5544357343
Danny Damsky <dannydamsky99@gmail.com>
2021-03-14 23:56:44 +0200

    Release 1.5.0

---
+++ src/Contract/ComponentRegistryInterface.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * ComponentRegistryInterface.php
+ *
+ * Copyright 2021 Danny Damsky
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package coffeephp\component-registry
+ * @author Danny Damsky <dannydamsky99@gmail.com>
+ * @since 2021-03-14
+ */
+
+declare(strict_types=1);
+
+namespace CoffeePhp\ComponentRegistry\Contract;
+
+use Psr\Container\ContainerExceptionInterface;
+
+/**
+ * Interface ComponentRegistryInterface
+ * @package coffeephp\component-registry
+ * @author Danny Damsky <dannydamsky99@gmail.com>
+ * @since 2021-03-14
+ */
+interface ComponentRegistryInterface
+{
+    /**
+     * Register a component.
+     *
+     * @param class-string<ComponentRegistrarInterface> $id
+     * The identifier of the component registrar class.
+     *
+     * @throws ContainerExceptionInterface
+     *
+     * @psalm-suppress InvalidThrow
+     */
+    public function register(string $id): void;
+}
